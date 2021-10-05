c7322d788a48390938afb127d7496d91786d9ab7
jue wang <joycewang@jues-MacBook-Pro.local>
2021-10-05 00:00:19 -0700

    model with 3,4,7 VGG layers with MSE loss and Adam optimizer

--- main_mse.py
+++ main_mse.py
@@ -8,7 +8,6 @@ import project_tests as tests
 import time
 
 
-# Check TensorFlow Version
 assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
 print('TensorFlow Version: {}'.format(tf.__version__))
 
